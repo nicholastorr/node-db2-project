9a2166c659cbbec2d15fc98d5484e9a9ad592839
nicholastorr <nick.torres213@gmail.com>
2021-09-19 23:55:01 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const carRouter = require('./cars/cars-router.js');
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json());
+
+server.use("/api/cars", carRouter);
 
 module.exports = server
